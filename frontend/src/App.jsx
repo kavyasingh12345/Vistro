8aa2354ca7e48aacfcca9e5184445db4434393c2
Kavya Singh <kavyasingh19052005@gmail.com>
2025-06-06 14:09:30 +0530

    user register and login backend done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,9 @@ import React from 'react'
 const App = () => {
   return (
     <div>
-      APP
+     <h1 className="text-3xl font-bold underline text-red-500">
+    He
+  </h1>
     </div>
   )
 }
---
+++ frontend/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",  // important for Vite root html
+    "./src/**/*.{js,jsx,ts,tsx}", // all your source files
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
